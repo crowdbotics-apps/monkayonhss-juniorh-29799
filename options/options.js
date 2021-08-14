8cf75ff8498392bdd8077ab21563e3b7339e3687
Crowdbotics <team@crowdbotics.com>
2021-08-14 03:36:25 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "monkayonhss_juniorh_29799Identifier",
+  url: "https://monkayonhss_juniorh_29799Identifier.botics.co",
+  api: "https://monkayonhss_juniorh_29799Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
